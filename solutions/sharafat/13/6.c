804032cc87ec04f6ba0ac646ecdebe7cfba92466
SharafatKarim <sharafat2004@gmail.com>
2023-08-18 16:01:15 +0600

    13 | 6 | sharafat

--- solutions/sharafat/13/6.c
+++ solutions/sharafat/13/6.c
@@ -4,8 +4,15 @@ int main()
 {
     FILE *fp1, *fp2;
     char ch, exclude;
-    fp1 = fopen("file1.txt", "r");
-    fp2 = fopen("file2.txt", "w");
+
+    char name1[20], name2[20];
+    printf("Enter file 1 name: ");
+    scanf("%s%*c", name1);
+    printf("Enter file 2 name: ");
+    scanf("%s%*c", name2);
+
+    fp1 = fopen(name1, "r");
+    fp2 = fopen(name2, "w");
     printf("What to exclude? ");
     scanf("%c", &exclude);
     while ((ch = fgetc(fp1)) != EOF)
